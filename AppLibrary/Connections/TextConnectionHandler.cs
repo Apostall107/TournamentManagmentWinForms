e994d9085ff9f2d90bcf1eb4a70ecac7fbe21f4c
Hryn Oleksandr <Apostall107@users.noreply.github.com>
2021-05-31 04:52:05 +0300

    Text connection Handler added some text connector settnigs implemented

---
+++ AppLibrary/Connections/TextConnectionHandler.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLibrary.Connections.TextConnectionHandler
+{
+    public static class TextConnectionHandler
+    {
+
+        public static string FullTxtFilePath(this string fileName)
+        {
+
+            return $"{ ConfigurationManager.AppSettings["FilePath"] }\\{fileName}";
+
+        }
+
+        public static List<string> LoadFile(this string file)
+        {
+
+            if (!File.Exists(file))
+            {
+
+                return new List<string>();
+
+            }
+
+            return File.ReadAllLines(file).ToList();
+
+
+
+
+        }
+
+
+        public static List<Models.PrizeModel> ConvertToPrizeModels(this List<string> lines)
+        {
+
+            List<Models.PrizeModel> output = new List<Models.PrizeModel>();
+
+            foreach (string ln in lines)
+            {
+
+                string[]  cols = ln.Split(',');
+
+                Models.PrizeModel p = new Models.PrizeModel();
+                p.ID = int.Parse(cols[0]);
+                p.PlaceNumber = int.Parse(cols[1]);
+                p.PlaceName = cols[2];
+                p.PrizeAmount = decimal.Parse(cols[3]);
+                p.PrizePercentage = double.Parse(cols[1]);
+                output.Add(p);
+
+            }
+
+            return output;
+        }
+
+
+        public static void SaveToPrizeFile(this List<Models.PrizeModel> models, string fileName)
+        {
+
+
+            List<string> ln = new List<string>();
+
+            foreach (Models.PrizeModel p in models)
+            {
+
+                ln.Add($"{ p.ID},{p.PlaceNumber },{ p.PlaceName},{p.PrizeAmount},{p.PrizePercentage}");
+            
+            }
+
+            File.WriteAllLines(fileName.FullTxtFilePath(), ln);
+
+        }
+
+
+    }
+
+}
--- AppLibrary/Connections/TextConnector.cs
+++ AppLibrary/Connections/TextConnector.cs
@@ -1,18 +1,28 @@
-﻿using AppLibrary.Models;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AppLibrary.Connections.TextConnectionHandler;
 
 namespace AppLibrary.Connections
 {
     class TextConnector : IDataConnection
     {
-        // TODO - realize method.
-        public PrizeModel CreatePrize(PrizeModel model)
+        // TODO - realize method. Connect the CreateNewPrize for txt file.
+
+
+        private const string PrizesFile = "PrizeModels.csv";
+
+
+        public Models.PrizeModel CreatePrize(Models.PrizeModel model)
         {
-            model.ID = 1;
+           List<Models.PrizeModel> prizes  =  PrizesFile.FullTxtFilePath().LoadFile().ConvertToPrizeModels();//load file and convert it to List
+
+            int currentID = prizes.OrderByDescending(x => x.ID).First().ID + 1;  // incrementation of ID
+
+            prizes.Add(model);//add new recorn with incremented ID
 
             return model;
         }
